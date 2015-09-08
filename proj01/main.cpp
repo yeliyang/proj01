e6593375204318abb7e1dc638ee7e7fa8b487246
Liyang Ye <gator@host-116977.dhcp.egr.msu.edu>
2015-09-08 10:29:20 -0400

    Initial Commit

---
+++ proj01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proj01
+//
+//  Created by Liyang Ye on 15/9/8.
+//  Copyright (c) 2015年 Liyang Ye. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
